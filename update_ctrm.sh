df977cee566e9a961a1333664b4d7f28f8db3f4a
Mr-Singh-69 <navdeep13dps@gmail.com>
2025-06-06 14:39:00 +0530

    umm added auto updater

---
+++ update_ctrm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to Cryptonium directory and update the repository
+cd ~/Cryptonium || { echo "Directory ~/Cryptonium not found."; exit 1; }
+git pull origin main
+
+# Return to home directory
+cd ~
+
+# Restart and check the cryptonium.service
+systemctl --user stop cryptonium.service
+systemctl --user start cryptonium.service
